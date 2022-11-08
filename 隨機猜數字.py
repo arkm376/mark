41dc55c46ad4f6240737d87b709c36072d77baa3
arkm376 <82326447+arkm376@users.noreply.github.com>
2022-11-08 23:47:53 +0800

    Update 隨機猜數字.py

--- 隨機猜數字.py
+++ 隨機猜數字.py
@@ -1,12 +1,12 @@
 import random
 
-print("隨機亂猜數字,猜對沒獎,可輸入最大最小")
-x = int(input("最小值?"))
-y = int(input("最大值?"))
-p = input("你的答案是?")
+print("Randomly guess numbers, no prizes for guessing right, you can enter the maximum and minimum")
+x = int(input("minimum value?"))
+y = int(input("maximum value?"))
+p = input("your anwsrs is?")
 a = str((random.randint(x,y)))
 if p == a :
-    print("你答對了喔!")
+    print("You got it right!")
 else:
-    print("猜錯囉~")
-print("答案是"+ a)
+    print("guess wrong")
+print("the answer is"+ a)
